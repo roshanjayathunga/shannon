56446f050267757c54b7c5757e16be6587770c30
Roshan Jayathunga <RoshanJ@hemantharobrandix.onmicrosoft.com>
2021-02-13 11:35:34 +0530

    Add programs into CMS configurations

--- src/pages/program/index.js
+++ src/pages/program/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
-export default class CourseIndexPage extends React.Component {
+export default class ProgramIndexPage extends React.Component {
   render() {
     return (
       <Layout>
