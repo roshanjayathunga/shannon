01e2b836ab4ae6aec8d1119a761dda4f542bed16
Roshan Jayathunga <RoshanJ@hemantharobrandix.onmicrosoft.com>
2021-02-23 07:55:05 +0530

    footer fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,73 +12,7 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer-section">
-        <Grid container
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >
-          <Grid item xs={12} sm={12} >
-            <h3 className="has-text-weight-semibold is-size-2 is-size-4-mobile is-size-2-tablet is-size-1-widescreen intro-heading">
-              <span className="footer-title">Sesquipedalian</span>
-            </h3>
-          </Grid>
-        </Grid>
-
-        <Grid container>
-          <Grid item xs={12} md={4} >
-            {/* <ul className="menu-list">
-              <li>
-                <Link to="/" className="navbar-item">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="navbar-item">
-                  About Us
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="navbar-item">
-                  On Stage and Beyond
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="navbar-item">
-                  Testimonials
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="navbar-item">
-                  Gallery
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="navbar-item">
-                  Contact Us
-                </Link>
-              </li>
-            </ul> */}
-          </Grid>
-
-          <Grid item xs={12} md={4} >
-            <a title="facebook" href="https://facebook.com" className="social-icon">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a title="facebook" href="https://facebook.com" className="social-icon">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a title="facebook" href="https://facebook.com" className="social-icon">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </Grid>
-
-          <Grid>
-            <Grid item xs={12} md={4} >
-
-            </Grid>
-          </Grid>
-
-        </Grid>
+       <p>ssdas</p>
       </footer>
     )
   }
