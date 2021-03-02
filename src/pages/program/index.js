989215d419753e5db54bdc96b98fb42e881c726e
Roshan Jayathunga <RoshanJ@hemantharobrandix.onmicrosoft.com>
2021-03-02 22:34:10 +0530

    program page

--- src/pages/program/index.js
+++ src/pages/program/index.js
@@ -36,11 +36,26 @@ export default class ProgramIndexPage extends React.Component {
         </section> */}
 
         <section className="section-program">
-          <h3 className="has-text-weight-semibold is-size-2 is-size-4-mobile is-size-2-tablet is-size-1-widescreen">
-            Our Programmes
-          </h3>
-
-          <div className="program-wrap mt-2">
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={12} md={9}
+            >
+              <h3 className="has-text-weight-semibold is-size-2 is-size-4-mobile is-size-2-tablet is-size-1-widescreen">
+                Our Programmes
+            </h3>
+            </Grid>
+            <Grid item xs={12} sm={12} md={3}>
+              <button variant="contained" color="primary" className="btn btn-primary float-right btm-sm-full-width">
+                Check Weekly Shedule
+            </button>
+            </Grid>
+          </Grid>
+          <div id="launch" className="program-wrap mt-2">
             <Grid
               container
               direction="row"
@@ -105,7 +120,7 @@ export default class ProgramIndexPage extends React.Component {
             </Grid>
           </div>
 
-          <div className="program-wrap mt-2">
+          <div id="launch" className="program-wrap mt-2">
             <Grid
               container
               direction="row"
@@ -138,7 +153,7 @@ export default class ProgramIndexPage extends React.Component {
               <Grid item xs={12} sm={12} md={3}>
                 <div
                   style={{
-                    backgroundImage: `url('https://images.pexels.com/photos/1815257/pexels-photo-1815257.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')`,
+                    backgroundImage: `url('https://images.pexels.com/photos/764681/pexels-photo-764681.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260')`,
                     width: '100%',
                     height: '80vh',
                     backgroundSize: 'cover',
@@ -147,7 +162,135 @@ export default class ProgramIndexPage extends React.Component {
                 >
                 </div>
               </Grid>
+            </Grid>
+          </div>
 
+          <div className="program-wrap mt-2">
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Grid item xs={12} sm={12} md={3}>
+                <div
+                  style={{
+                    backgroundImage: `url('https://images.pexels.com/photos/1720186/pexels-photo-1720186.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260')`,
+                    width: '100%',
+                    height: '80vh',
+                    backgroundSize: 'cover',
+                    backgroundPositionX: 'center'
+                  }}
+                >
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={12} md={9}>
+                <div className="program-info">
+                  <h3 className="has-text-weight-semibold is-size-3 is-size-5-mobile is-size-3-tablet is-size-3-widescreen">
+                    Propel (For ages 11-13)
+                  </h3>
+                  <p>
+                    At this stage, Students will develop word awareness, learn morphological
+                    elements that are most frequent and Independent word learning strategies
+                    through a wide genre of age appropriate literary content. Additionally
+                    students will develop the ability to use context to determine meanings of
+                    unknown words  through carefully structured assignments and Collaborated
+                    Learning activities
+                  </p>
+                  <br />
+
+                  <p>
+                    Lessons will consist of performance-led activities drawing awareness
+                    to emotion, mood and the ability to  communicate this understanding
+                    through the use of vocal techniques including  breath control,
+                    pause, emphasis, changes of tone, pace, pitch and volume.
+                  </p>
+
+                  <br />
+                  <p>
+                    Students will be guided to develop communication skills through
+                    the delivery of effective presentations on social, cultural or environmental
+                    issues that demonstrate background research, analysis and debate while integrating
+                    presentation skills with clear, relevant visual aids.
+                  </p>
+                  <br />
+
+                  <p>
+                    Drama led activities including hot seating, devised drama, role play
+                    improvisation and SEL strategies (social and emotional learning) will
+                    present learners with nine essential leadership skills to build
+                    character and competence needed for success.
+                  </p>
+                </div>
+
+              </Grid>
+            </Grid>
+          </div>
+
+          <div className="program-wrap mt-2">
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              spacing={4}
+            >
+              <Grid item xs={12} sm={12} md={9}>
+                <div className="program-info">
+                  <h3 className="has-text-weight-semibold is-size-3 is-size-5-mobile is-size-3-tablet is-size-3-widescreen">
+                    Ascend (For ages 14 and above)
+                  </h3>
+                  <p>
+                    At the end of this course students will use multisyllabic words or
+                    phrase choices based on a highly developed vocabulary and a thorough
+                    understanding of challenging contexts. Performance led activities will
+                    enable students to acquire the skill to communicate the meaning of a
+                    given text with relevant and appropriate use of vocal and physical
+                    techniques to engage an audience fully.
+                  </p>
+                  <br />
+
+                  <p>
+                    Students who progress to the ‘Ascend’ stage  will demonstrate the
+                    ability to engage spontaneously and take some responsibility for
+                    discussion, including appropriate justification of opinions and
+                    showing some perceptiveness and active listening skills through
+                    debate, role play and real-play.
+                  </p>
+                  <br />
+
+                  <p>
+                    Learners will develop the ability to deliver a talk on an unseen
+                    topic while demonstrating knowledge of the subject matter and
+                    engaging interactively with the audience through verbal and
+                    non-verbal communication.
+                  </p>
+                  <br />
+
+                  <p>
+                    The program directs students to develop a creative response to
+                    challenges in authentic situations, identify individual personality
+                    traits and develop emotional intelligence through the exploration of skits,
+                    improvisation and relevant theatre techniques.
+                  </p>
+
+
+                </div>
+              </Grid>
+
+              <Grid item xs={12} sm={12} md={3}>
+                <div
+                  style={{
+                    backgroundImage: `url('https://images.pexels.com/photos/4834822/pexels-photo-4834822.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')`,
+                    width: '100%',
+                    height: '80vh',
+                    backgroundSize: 'cover',
+                    backgroundPositionX: 'center'
+                  }}
+                >
+                </div>
+              </Grid>
             </Grid>
           </div>
 
