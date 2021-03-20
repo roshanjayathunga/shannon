bf19945ecc0ce82cd356170d8743aaf37c483d6d
Roshan Jayathunga <RoshanJ@hemantharobrandix.onmicrosoft.com>
2021-03-20 07:37:10 +0530

    on stage page

--- src/pages/onstagebeyond/index.js
+++ src/pages/onstagebeyond/index.js
@@ -1,239 +1,149 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
-import Layout from '../../components/Layout'
+import Layout from '../../components/Layout';
+import { Link } from 'gatsby'
 
 export default class ProgramIndexPage extends React.Component {
 
   render() {
     return (
       <Layout>
-        <section className="section-about">
-          <div className="about-wrap">
-            <h3 className="has-text-weight-semibold is-size-2 is-size-4-mobile is-size-2-tablet is-size-2-widescreen">
-              On Stage and Beyond
-            </h3>
+        <section className="section-onstage">
 
-            <h3 className="has-text-weight-semibold is-size-3 is-size-5-mobile is-size-3-tablet is-size-3-widescreen">
-              Performances
-          </h3>
+          {/* Section 1 */}
+          <Grid container
+            direction="row"
+            justify="center"
 
-            <p>
-              Training for performance is an important element included in the enrichment program,
-              students are trained each year to present a large scale theatrical
-              performance staged in a professional theatre to a paying  audience,
-              the play is rehearsed over the term and performed as a year end event.
-          </p>
-            <br />
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-              spacing={4}
-            >
-              <Grid item xs={12} sm={12} md={6}
-              >
-                <div
-                  style={{
-                    backgroundImage: `url('https://images.pexels.com/photos/5149654/pexels-photo-5149654.jpeg?cs=srgb&dl=pexels-budgeron-bach-5149654.jpg&fm=jpg')`,
-                    width: '100%',
-                    height: '80vh',
-                    backgroundSize: 'cover',
-                    backgroundPositionX: 'center'
-                  }}
-                >
-                </div>
-              </Grid>
-              <Grid item xs={12} sm={12} md={6}>
-                <div
-                  style={{
-                    backgroundImage: `url('https://images.pexels.com/photos/6936086/pexels-photo-6936086.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500')`,
-                    width: '100%',
-                    height: '80vh',
-                    backgroundSize: 'cover',
-                    backgroundPositionX: 'center'
-                  }}
-                >
-                </div>
-              </Grid>
+          >
+            <Grid item xs={12} sm={12} md={6} >
+              <h1 className="main-title mt-4 mr-2 text-right">Classmates to</h1>
             </Grid>
-          </div>
+            <Grid item xs={12} sm={12} md={6} >
+              <h1 className="main-title">castmates.</h1>
+              <h2 className="main-sub-title">Join the theatre company</h2>
+            </Grid>
+          </Grid>
 
+          <Grid container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={3}
 
-          <div>
-            <div className="about-mission">
-              <h3 className="has-text-weight-semibold is-size-3 is-size-5-mobile is-size-3-tablet is-size-3-widescreen">
-                Enrichment workshops
-          </h3>
-              <p >Enrichment workshops coincide with termly holidays and are designed to
-              provide children with an opportunity to collaborate in open-ended
-              challenges enabling students to learn and experience the creative
-              process while fostering their imagination and curiosity.  Each season,
-              we offer new and engaging ‘Team Challenges’ that will encourage your
-              child to explore their passions, discover their unique talents and
-              learn new skills. Teams also participate in Instant Challenges—quick
-                  creative and critical thinking exercises that build teamwork and problem solving abilities.</p>
-              <br />
+          >
+            <Grid item xs={12} sm={12} md={3} >
+              <Link to="/gallery">
+                <button variant="contained" color="primary" className="btn btn-primary float-right btm-sm-full-width">
+                  View Gallery
+            </button>
+              </Link>
+            </Grid>
+            <Grid item xs={12} sm={12} md={9} >
+              <p className="page-description">The skillset of a performer translates to any profession. Whether it’s improvisation, collaboration or communication, students employ a diverse range of stage techniques in their everyday experiences. </p>
+              <p className="page-description mt-1">In a changing world, students at Sesquipedalian will be better equipped to communicate purposefully, think creatively and critically, build emotional intelligence and manage interpersonal relationships with proficiency and care.</p>
+            </Grid>
+          </Grid>
 
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Young Explorers challenge
-            </h4>
-              <p>Young explorers challenges are customized for early learners to  explore
-              their creativity and skills through dramatic adventures. Students find a
-                safe place to work together and make friends. </p>
-              <br />
+          {/* Section 2 */}
+          <Grid container
+            direction="row"
+            justify="center"
+            className="mt-3"
 
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Fine Arts Challenge
-            </h4>
-              <p>Fine Arts Challenges allow students to explore social issues while experimenting
-              with aspects of visual arts
-                including script writing, stage design, props costume and artistic media. </p>
-              <br />
+          >
+            <Grid item xs={12} sm={12} md={6} >
+              <h1 className="main-title mt-4 mr-2 text-right">Take an act</h1>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6} >
+              <h1 className="main-title">break.</h1>
+              <h2 className="main-sub-title">Enrichment workshops</h2>
+            </Grid>
+          </Grid>
 
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Improvisational challenge
-            </h4>
-              <p>Improvisational Challenges are focused on spontaneity and storytelling.
-                Teams receive topics and props to create on the spot.</p>
-              <br />
+          <Grid container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={3}
 
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Community service challenge
-            </h4>
-              <p>Community service challenges allow students to identify, design, plan, and carry out a
-                project that addresses an existing community need</p>
-              <br />
+          >
+            <Grid item xs={12} sm={12} md={3} >
+            </Grid>
+            <Grid item xs={12} sm={12} md={9} >
+              <p className="page-description">The skillset of a performer translates to any profession. Whether it’s improvisation, collaboration or communication, students employ a diverse range of stage techniques in their everyday experiences. </p>
+              <p className="page-description mt-1">In a changing world, students at Sesquipedalian will be better equipped to communicate purposefully, think creatively and critically, build emotional intelligence and manage interpersonal relationships with proficiency and care.</p>
+            </Grid>
+          </Grid>
 
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Scientific Challenge
-            </h4>
-              <p>Scientific Challenges are a blend of research and curiosity of
-                science with the thrill and creativity of the theater arts.</p>
-              <br />
+          {/* Section 3 */}
+          <Grid container
+            direction="row"
+            justify="center"
+            spacing={3}
+            className="mt-3 bg-secondary onstage-card"
+          >
+            <Grid item xs={12} sm={12} md={12} >
+              <h1>Young Explorers Challenge</h1>
+              <p>A project that brings younger students together to test their creativity through Team Challenges.</p>
+            </Grid>
+          </Grid>
 
-            </div>
-          </div>
+          <Grid container
+            direction="row"
+            justify="center"
+            spacing={3}
+            className="mt-3 bg-primary onstage-card"
+          >
+            <Grid item xs={12} sm={12} md={12} >
+              <h1>Fine Arts Challenge</h1>
+              <p>Students explore social issues while experimenting with
+              aspects of visual arts including script writing, stage design,
+              Props costume and artistic media.
+              </p>
+            </Grid>
+          </Grid>
 
-          <br /><br /><br /><br />
+          <Grid container
+            direction="row"
+            justify="center"
+            spacing={3}
+            className="mt-3 bg-accent onstage-card"
+          >
+            <Grid item xs={12} sm={12} md={12} >
+              <h1>Improvisational Challenge</h1>
+              <p>This Team Challenge focuses on spontaneity and storytelling,with teams receiving props and topics on the spot.
+              </p>
+            </Grid>
+          </Grid>
 
+          <Grid container
+            direction="row"
+            justify="center"
+            spacing={3}
+            className="mt-3 bg-secondary onstage-card"
+          >
+            <Grid item xs={12} sm={12} md={12} >
+              <h1>Community Service Challenge</h1>
+              <p>Scientific Challenges combine research and curiosity of 
+					science with the creativity of theater arts.</p>
+            </Grid>
+          </Grid>
 
-          <div>
-            <div className="about-mission">
-              <h3 className="has-text-weight-semibold is-size-3 is-size-5-mobile is-size-3-tablet is-size-3-widescreen">
-                Enrichment workshops
-          </h3>
-              <p style={{ fontSize: '16px' }} >Enrichment workshops coincide with termly holidays and are designed to
-              provide children with an opportunity to collaborate in open-ended
-              challenges enabling students to learn and experience the creative
-              process while fostering their imagination and curiosity.  Each season,
-              we offer new and engaging ‘Team Challenges’ that will encourage your
-              child to explore their passions, discover their unique talents and
-              learn new skills. Teams also participate in Instant Challenges—quick
-                  creative and critical thinking exercises that build teamwork and problem solving abilities.</p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Young Explorers challenge
-            </h4>
-              <p style={{ fontSize: '16px', textAlign:'justify' }} >Young explorers challenges are customized for early learners to  explore
-              their creativity and skills through dramatic adventures. Students find a
-                safe place to work together and make friends. </p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Fine Arts Challenge
-            </h4>
-              <p style={{ fontSize: '16px', textAlign:'justify' }}>Fine Arts Challenges allow students to explore social issues while experimenting
-              with aspects of visual arts
-                including script writing, stage design, props costume and artistic media. </p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Improvisational challenge
-            </h4>
-              <p style={{ fontSize: '16px', textAlign:'justify' }}>Improvisational Challenges are focused on spontaneity and storytelling.
-                Teams receive topics and props to create on the spot.</p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Community service challenge
-            </h4>
-
-              <p style={{ fontSize: '16px', textAlign:'justify' }}>Community service challenges allow students to identify, design, plan, and carry out a
-                project that addresses an existing community need</p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Scientific Challenge
-            </h4>
-              <p style={{ fontSize: '16px', textAlign:'justify' }}>Scientific Challenges are a blend of research and curiosity of
-                science with the thrill and creativity of the theater arts.</p>
-              <br />
-            </div>
-          </div>
-
-
-
-          <br /><br /><br /><br />
-
-
-          <div>
-            <div className="about-mission">
-              <h3 className="has-text-weight-semibold is-size-3 is-size-5-mobile is-size-3-tablet is-size-3-widescreen">
-                Enrichment workshops
-</h3>
-              <p style={{ fontSize: '21px',lineHeight:'32px' }} >Enrichment workshops coincide with termly holidays and are designed to
-              provide children with an opportunity to collaborate in open-ended
-              challenges enabling students to learn and experience the creative
-              process while fostering their imagination and curiosity.  Each season,
-              we offer new and engaging ‘Team Challenges’ that will encourage your
-              child to explore their passions, discover their unique talents and
-              learn new skills. Teams also participate in Instant Challenges—quick
-        creative and critical thinking exercises that build teamwork and problem solving abilities.</p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Young Explorers challenge
-  </h4>
-              <p style={{ fontSize: '21px',lineHeight:'32px' }} >Young explorers challenges are customized for early learners to  explore
-              their creativity and skills through dramatic adventures. Students find a
-      safe place to work together and make friends. </p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Fine Arts Challenge
-  </h4>
-              <p style={{ fontSize: '21px',lineHeight:'32px' }}>Fine Arts Challenges allow students to explore social issues while experimenting
-              with aspects of visual arts
-      including script writing, stage design, props costume and artistic media. </p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Improvisational challenge
-  </h4>
-              <p style={{ fontSize: '21px',lineHeight:'32px' }}>Improvisational Challenges are focused on spontaneity and storytelling.
-      Teams receive topics and props to create on the spot.</p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Community service challenge
-  </h4>
-
-              <p style={{ fontSize: '21px',lineHeight:'32px' }}>Community service challenges allow students to identify, design, plan, and carry out a
-      project that addresses an existing community need</p>
-              <br />
-
-              <h4 className="has-text-weight-semibold is-size-4 is-size-4-mobile is-size-4-tablet is-size-4-widescreen">
-                Scientific Challenge
-  </h4>
-              <p style={{ fontSize: '21px',lineHeight:'32px' }}>Scientific Challenges are a blend of research and curiosity of
-      science with the thrill and creativity of the theater arts.</p>
-              <br />
-            </div>
-          </div>
-
-
+          <Grid container
+            direction="row"
+            justify="center"
+            spacing={3}
+            className="mt-3 bg-primary onstage-card"
+          >
+            <Grid item xs={12} sm={12} md={12} >
+              <h1>Scientific Challenge</h1>
+              <p>Students explore social issues while experimenting with
+              aspects of visual arts including script writing, stage design,
+              Props costume and artistic media.
+              </p>
+            </Grid>
+          </Grid>
 
         </section>
       </Layout>
